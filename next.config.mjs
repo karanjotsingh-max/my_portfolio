498cc99462c97189985a5ea502745a02a9f5facf
karanjotsingh-max <abh88138@gmail.com>
2025-10-25 18:00:42 -0600

    changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,5 @@
-// next.config.mjs
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',              // produce static site into /out
-  images: { unoptimized: true }, // GH Pages has no image optimizer
-  // ❌ No basePath/assetPrefix for user site
-  // trailingSlash: true, // optional; enable only if you prefer /path/ URLs
+export default {
+  output: 'export',
+  images: { unoptimized: true },
 };
-export default nextConfig;
